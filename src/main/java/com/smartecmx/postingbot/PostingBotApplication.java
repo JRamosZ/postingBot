c7dc7fb7563fcb7b8c2bb631e09955c2effd49ae
jorge_ramos <zramos.ivan@gmail.com>
2025-07-07 10:26:31 -0600

    first commit

---
+++ src/main/java/com/smartecmx/postingbot/PostingBotApplication.java
@@ -0,0 +1,11 @@
+package com.smartecmx.postingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingBotApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostingBotApplication.class, args);
+	}
+}
