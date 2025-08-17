29506bc3b6a866e8366e9ba51492c2598105e168
jorge_ramos <zramos.ivan@gmail.com>
2025-08-17 16:55:20 -0600

    Completed Reels published to Facebook

--- src/main/java/com/smartecmx/postingbot/util/FacebookUtil.java
+++ src/main/java/com/smartecmx/postingbot/util/FacebookUtil.java
@@ -1,12 +1,15 @@
 package com.smartecmx.postingbot.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,7 @@ import com.smartecmx.postingbot.exception.FacebookException;
 import com.smartecmx.postingbot.exception.PostingBotException;
 import com.smartecmx.postingbot.model.Token;
 import com.smartecmx.postingbot.model.Responses.FacebookPostResponse;
+import com.smartecmx.postingbot.model.Responses.FacebookVideoPostResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,6 +34,9 @@ public class FacebookUtil {
 
     @Value("${com.smartecmx.postingbot.util.facebook.facebook_video_post_url}")
     private String facebookVideoPostUrl;
+
+    @Value("${com.smartecmx.postingbot.util.facebook.facebook_reels_post_url}")
+    private String facebookReelsPostUrl;
 
     private final TokenUtil tokenUtil;
 
@@ -60,26 +67,45 @@ public class FacebookUtil {
         return response.getBody().getId();
     }
 
-    public String postFacebookVideo(String message, Path videoPath) throws PostingBotException {
+    public String postFacebookVideo(String message, Path videoPath) throws PostingBotException, IOException {
         RestTemplate rest = new RestTemplate();
 
         Token token = tokenUtil.getActiveTokenByType("page");
 
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-        params.add("video_file", new FileSystemResource(videoPath.toString()));
-        params.add("description", "description: " + message);
-        params.add("access_token", token.getValue());
+        MultiValueMap<String, Object> initBody = new LinkedMultiValueMap<>();
+        initBody.add("upload_phase", "start");
+        initBody.add("access_token", token.getValue());
+
+        ResponseEntity<Map> initResp = rest.postForEntity(facebookReelsPostUrl, new HttpEntity<>(initBody), Map.class);
+
+        String videoId = initResp.getBody().get("video_id").toString();
+        String uploadUrl = initResp.getBody().get("upload_url").toString();
+
+        byte[] fileBytes = Files.readAllBytes(videoPath);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.set("Content-Type", "application/octet-stream");
+        headers.set("offset", "0");
+        headers.set("file_size", String.valueOf(fileBytes.length));
+        headers.set("Authorization", "OAuth " + token.getValue());
 
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(params, headers);
+        HttpEntity<byte[]> transferEntity = new HttpEntity<>(fileBytes, headers);
 
-        ResponseEntity<FacebookPostResponse> response = rest.postForEntity(facebookVideoPostUrl, request, FacebookPostResponse.class);
+        rest.exchange(uploadUrl, HttpMethod.POST, transferEntity, String.class);
 
-        if (response.getStatusCode() != HttpStatus.OK) {
-            throw new FacebookException("Failed to post to Facebook: " + response.getBody());
+        MultiValueMap<String, Object> finishBody = new LinkedMultiValueMap<>();
+        finishBody.add("upload_phase", "finish");
+        finishBody.add("video_id", videoId);
+        finishBody.add("access_token", token.getValue());
+        finishBody.add("title", "Mi Reel");
+        finishBody.add("description", message);
+        finishBody.add("video_state", "PUBLISHED");
+
+        ResponseEntity<FacebookVideoPostResponse> finishResp = rest.postForEntity(facebookReelsPostUrl, new HttpEntity<>(finishBody), FacebookVideoPostResponse.class);
+        if (finishResp.getStatusCode() != HttpStatus.OK) {
+            throw new FacebookException("Failed to post Reel to Facebook: " + finishResp.getBody());
         }
-        return response.getBody().getId();
+
+        return finishResp.getBody().getPost_id();
     }
 }
