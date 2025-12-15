90b4dce6580b54ce2ab0ab4220a28cf0947dad7e
jorge_ramos <zramos.ivan@gmail.com>
2025-12-14 18:32:36 -0600

    Update overlay configurations for TECHNICALTIP_4 to improve layout and positioning

--- src/main/java/com/smartecmx/postingbot/common/OverlayConfig.java
+++ src/main/java/com/smartecmx/postingbot/common/OverlayConfig.java
@@ -16,8 +16,8 @@ public enum OverlayConfig {
     TECHNICALTIP_2_UUBFSU_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 50, 1020, 400, "center"),
     TECHNICALTIP_3_IQOJSC("Arial", 48, "bold", "#000000", "north_west", 140, 320, 800, "center"),
     TECHNICALTIP_3_IQOJSC_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 155, 1185, 400, "center"),
-    TECHNICALTIP_4_TCZA85("Arial", 48, "bold", "#000000", "north_west", 20, 20, 800, "center"),
-    TECHNICALTIP_4_TCZA85_CTA("Courier", 48, "bold", "#FFFFFF", "north_west", 20, 20, 800, "center"),
+    TECHNICALTIP_4_TCZA85("Arial", 56, "bold", "#000000", "north_west", 100, 300, 900, "left"),
+    TECHNICALTIP_4_TCZA85_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 280, 1268, 800, "center"),
     TECHNICALTIP_5_AQE4OQ("Arial", 48, "bold", "#000000", "north_west", 20, 20, 800, "center"),
     TECHNICALTIP_5_AQE4OQ_CTA("Courier", 48, "bold", "#FFFFFF", "north_west", 20, 20, 800, "center");
 
