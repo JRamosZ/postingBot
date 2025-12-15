7a36df09c8fbda59cfa989c9dab21f4f8905699a
jorge_ramos <zramos.ivan@gmail.com>
2025-12-14 18:54:49 -0600

    Finished overlays configuration

--- src/main/java/com/smartecmx/postingbot/common/OverlayConfig.java
+++ src/main/java/com/smartecmx/postingbot/common/OverlayConfig.java
@@ -10,15 +10,15 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum OverlayConfig {
     // This enum names must match the filenames of the images in Cloudinary (without extension)
-    TECHNICALTIP_1_XAYEGE("Arial", 52, "bold", "#000000", "north_west", 100, 250, 900, "right"),
+    TECHNICALTIP_1_XAYEGE("Arial", 58, "bold", "#000000", "north_west", 70, 220, 900, "right"),
     TECHNICALTIP_1_XAYEGE_CTA("Courier", 41, "bold", "#FFFFFF", "north_west", 280, 1270, 800, "center"),
-    TECHNICALTIP_2_UUBFSU("Arial", 48, "bold", "#000000", "north_west", 100, 400, 800, "left"),
+    TECHNICALTIP_2_UUBFSU("Arial", 48, "bold", "#000000", "north_west", 100, 400, 750, "left"),
     TECHNICALTIP_2_UUBFSU_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 50, 1020, 400, "center"),
     TECHNICALTIP_3_IQOJSC("Arial", 48, "bold", "#000000", "north_west", 140, 320, 800, "center"),
     TECHNICALTIP_3_IQOJSC_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 155, 1185, 400, "center"),
-    TECHNICALTIP_4_TCZA85("Arial", 56, "bold", "#000000", "north_west", 100, 300, 900, "left"),
+    TECHNICALTIP_4_TCZA85("Arial", 56, "bold", "#000000", "north_west", 100, 300, 900, "center"),
     TECHNICALTIP_4_TCZA85_CTA("Courier", 43, "bold", "#FFFFFF", "north_west", 280, 1268, 800, "center"),
-    TECHNICALTIP_5_AQE4OQ("Arial", 56, "bold", "#FFFFFF", "north_west", 100, 350, 430, "left"),
+    TECHNICALTIP_5_AQE4OQ("Arial", 58, "bold", "#FFFFFF", "north_west", 100, 350, 430, "left"),
     TECHNICALTIP_5_AQE4OQ_CTA("Courier", 45, "bold", "#000000", "north_west", 280, 1268, 800, "center");
 
     private final String font;
